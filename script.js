0c74b8a9fcee86eaeacc13804346e4ec6fb44f2e
Lostchild-sudo <lostchild0809@gmail.com>
2026-03-06 18:49:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ page.style.display = "none";
 document.getElementById(pageId).style.display = "block";
 
 }
+
+function likePost(button){
+
+if(button.innerText === "❤️ Like"){
+button.innerText = "💔 Unlike";
+}else{
+button.innerText = "❤️ Like";
+}
+
+}

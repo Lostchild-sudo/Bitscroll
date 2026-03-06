5a12abc6be7d8cf2edd620a1bb3055bcc3041dc7
Lostchild-sudo <lostchild0809@gmail.com>
2026-03-06 18:44:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function showPage(pageId){
 
+let pages = document.querySelectorAll("main section");
+
+pages.forEach(function(page){
+page.style.display = "none";
+});
+
+document.getElementById(pageId).style.display = "block";
+
+}

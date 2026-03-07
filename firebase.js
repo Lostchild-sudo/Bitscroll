3cb6597d2c73b99a29cc5eae2eb5418552692875
Lostchild-sudo <lostchild0809@gmail.com>
2026-03-07 14:58:20 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBxrbh42fJ_JDGFrErSRA2AuFB56IdnLzw",
+  authDomain: "bitscroll-34890.firebaseapp.com",
+  projectId: "bitscroll-34890",
+  storageBucket: "bitscroll-34890.firebasestorage.app",
+  messagingSenderId: "1074276995159",
+  appId: "1:1074276995159:web:dcaabdea6f8662c253d312"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
